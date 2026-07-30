2036d3556aaa270863d2114dfc4e1c623980209f
remi888abg <remi-lion@hotmail.com>
2026-07-31 00:10:13 +0800

    update sw to v19 with 4 chunks

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker：单文件离线版专用。缓存应用外壳 + 主 HTML，实现“添加到主屏幕后离线可用”
 // 策略：stale-while-revalidate（先返回缓存，同时后台刷新缓存），保证离线即时可用、线上更新能自动下发
-const CACHE = 'dandan-bridge-offline-v18';
-const PRECACHE = ['./manifest.webmanifest', './icon-192.png', './icon-512.png', './favicon.ico', './app-core.js'];
+const CACHE = 'dandan-bridge-offline-v19';
+const PRECACHE = ['./manifest.webmanifest', './icon-192.png', './icon-512.png', './favicon.ico', './app-core-1.js', './app-core-2.js', './app-core-3.js', './app-core-4.js'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(PRECACHE).catch(() => {})).then(() => self.skipWaiting()));
